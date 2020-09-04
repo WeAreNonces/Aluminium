b363a12dc09ff38afd607387d55549ec208ae5d8
viown <48097677+viown@users.noreply.github.com>
2020-09-04 08:53:01 +0300

    Added config parsing

---
+++ Aluminium/Aluminium/configparser.cpp
@@ -0,0 +1,67 @@
+#include "configparser.h"
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <cctype>
+
+// Forward declaring split function from Discord.cpp
+std::vector<std::string> split(std::string str, char separator);
+
+std::string strip(std::string s) {
+    while (s.front() == ' ' || s.back() == ' ') {
+        if (s.front() == ' ')
+            s.erase(0, 1);
+        if (s.back() == ' ')
+            s.pop_back();
+    }
+    return s;
+}
+
+bool isdigit(std::string s) {
+    for (char c : s) {
+        if (std::isdigit(c) == 0)
+            return false;
+    }
+    return true;
+}
+
+// Parses the file
+// Possible errors:
+// "OP_CONFIG_OPEN_FAILURE" -> Configuration file not found or failed to open
+config::parser config::parse(std::string filename) {
+    config::parser f = {};
+	std::ifstream config{"config.txt"};
+	if (!config)
+		throw "OP_CONFIG_OPEN_FAILURE";
+	while (config) {
+        std::string line;
+        std::getline(config, line);
+		if (line[0] == '#')
+			continue;
+        std::vector<std::string> s = split(line, '=');
+        if (s.size() == 2)
+            f[strip(s[0])] = strip(s[1]);
+        else
+            continue;
+	}
+    return f;
+}
+
+config::ParseValue config::get(config::parser ParseObject, std::string key) {
+    try {
+        ParseObject.at(key);
+    }
+    catch(std::out_of_range) {
+        return config::InvalidValue();
+    }
+    std::string value = ParseObject[key];
+    // Before you complain cube, switch() only allows int, enum, char, etc.
+    if (value == "true")
+        return true;
+    else if (value == "false")
+        return false;
+    else if (isdigit(value))
+        return std::stoi(value);
+    return value;
+}
--- Aluminium/Aluminium/configparser.h
+++ Aluminium/Aluminium/configparser.h
@@ -1,8 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <variant> // C++17
 #ifndef _CONFIG
 #define _CONFIG
 
 namespace config {
-
+	class InvalidValue {};
+	typedef std::map<std::string, std::string> parser;
+	typedef std::variant<int, bool, std::string, InvalidValue> ParseValue;
+	// Parses the configuration file and returns a parser
+	// View configparser.cpp for better documentation
+	config::parser parse(std::string filename);
+	// Gets the value, while parse objects can be retrieved through config::parser[key], this handles values better.
+	// This is more recommended than config::parser[key].
+	// Returns config::InvalidValue if the key is incorrect.
+	// Important note: value must be fetched through std::get, ask vi for more information
+	ParseValue get(config::parser ParseObject, std::string key);
 }
 
 #endif
