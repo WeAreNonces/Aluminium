cbde90b4537a7d9ef8642c5ab75c73d4e19dbf37
Cube <70229983+Cube9999@users.noreply.github.com>
2020-09-06 00:24:35 -0500

    Added a cmd handler

--- Aluminium/Aluminium/Aluminium.cpp
+++ Aluminium/Aluminium/Aluminium.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <tuple>
+#include <Windows.h>
 
 // Returns the amount of threads needed
 int alm::getThreadCount(int tokens) {
@@ -61,5 +62,65 @@ void alm::joinServer(std::string invite) {
     for (auto token : tokens) {
         discord::Account asd{ token };
         asd.joinserver(invite);
+    }
+}
+template<typename T>
+void alm::print(T msg) {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 4);
+    std::cout << "[ ALUMINUM ] ";
+    SetConsoleTextAttribute(h, 15);
+    std::cout << msg;
+}
+
+void alm::ParseCmds(std::string cmd) {
+
+    std::vector<std::string> args = alm::split(cmd, ' ');
+
+    if (args[0] == "help") {
+        alm::print("Here are the commands available: ");
+        std::cout << R"(   
+        help - prints out this message
+        join [invite] - makes the bots join a server
+        config - Opens up config.txt
+        tokens - Opens up tokens.txt
+        raid [channelid] - spams messages to a channel
+        leave [guildid] - Leaves a server
+        sendfriendrequest [userid] - Sends a friend request to a user
+)";
+    }
+
+    else if (args[0] == "join") {
+        if (args.size() == 2) {
+            // ur code goes here, vi
+        }
+        else {
+            alm::print("join [invite] - Makes the bots join a server\n");
+        }
+    }
+
+    else if (args[0] == "sendfriendrequest" || args[0] == "spamfr") {
+        if (args.size() == 2) {
+            // ur code goes here, vi
+        }
+        else {
+            alm::print("sendfriendrequest [userid] - Makes the bots join a server\n");
+        }
+    }
+
+    else if (args[0] == "raid") {
+        if (args.size() == 2) {
+            // ur code goes here, vi
+        }
+        else {
+            alm::print("raid [channelid] - Makes the bots join a server\n");
+        }
+    }
+
+    else if (args[0] == "config") {
+        system("start config.txt");
+    }
+    else if (args[0] == "tokens") {
+        system("start tokens.txt");
     }
 }
--- Aluminium/Aluminium/Main.cpp
+++ Aluminium/Aluminium/Main.cpp
@@ -2,18 +2,15 @@
 #include "Discord.h"
 #include "Aluminium.h"
 #include <thread>
+#include <string>
 
 
 int main() {
-	/*
-		Visual Studio's Intellisense keeps having a fucking seizure
-		So working on this will probably take me 3 hours. YAY!
-	*/
-	std::vector<std::string> tokens = alm::getTokens("tokens.txt"); // This will read all of our tokens and store them in an std::vector
-	alm::joinServer("N3p4Ru");
-	for(int i = 0; i < alm::getThreadCount(tokens.size()); i++) {
-		std::thread t(alm::ServerRaid, 751591956597178402, "Test1234");
-		t.join();
-	}
+	do {
+		alm::print("Please input a command: ");
+		std::string cmd;
+		std::getline(std::cin, cmd);
+		alm::ParseCmds(cmd);
+	} while (true);
 	return 0;
 }
